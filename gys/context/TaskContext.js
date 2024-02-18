f49faa222d284dbafc8458f6be8e9c6312b6711b
Utku <utkuhazargel@gmail.com>
2024-02-19 00:49:34 +0300

    id ye göre silme işlemi eklendi

--- gys/context/TaskContext.js
+++ gys/context/TaskContext.js
@@ -4,17 +4,34 @@ import createDataContext from "./createDataContext";
 const taskReducer = (state, action) => {
   switch (action.type) {
     case "add_task":
-      return [...state, { title: "görev 4",content:"açıklama"}];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 999999),
+          title: "görev 5",
+          content: "açıklama",
+        },
+      ];
+    case "delete_task":
+      return state.filter((task)=> task.id !== action.payload)
     default:
       return state;
   }
 };
 
 const addTask = (dispatch) => {
-  return ()=>{
+  return () => {
     dispatch({ type: "add_task" });
-  }
-  
+  };
+};
+const deleteTask = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete_task", payload: id });
+  };
 };
 
-export const {Context,Provider}=createDataContext(taskReducer,{addTask},[]);
+export const { Context, Provider } = createDataContext(
+  taskReducer,
+  { addTask, deleteTask },
+  []
+);
--- gys/screens/HomeScreen.js
+++ gys/screens/HomeScreen.js
@@ -1,20 +1,24 @@
-import { StyleSheet, Text, View, FlatList,Button } from "react-native";
+import { StyleSheet, Text, View, FlatList,Button, Touchable, TouchableOpacity } from "react-native";
 import React, { useContext } from "react";
 import {Context} from "../context/TaskContext";
+import { Feather } from '@expo/vector-icons';
 
 export default function HomeScreen() {
-  const {state,addTask} = useContext(Context);
+  const {state,addTask,deleteTask} = useContext(Context);
   return (
     <View>
       <Button title="Ekle" onPress={addTask} />
       <FlatList
         data={state}
-        keyExtractor={task => task.title}
+        keyExtractor={task => task.id}
         renderItem={({ item }) => {
           return (
-            <View>
-              <Text>{item.title}</Text>
-              <Text>{item.content}</Text>
+            <View style={styles.taskContainer}>
+              <Text  style={styles.taskTitle}>{item.title}</Text>
+              
+              <TouchableOpacity onPress={()=>deleteTask(item.id)}>
+              <Feather  name="trash" size={24} color="black" />
+              </TouchableOpacity>
               
             </View>
           );
@@ -24,4 +28,17 @@ export default function HomeScreen() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  taskContainer:{
+    flex:1,
+    flexDirection:"row",
+    justifyContent:"space-between",
+    borderBottomWidth:1,
+    paddingHorizontal:10,
+    paddingVertical:20,
+    borderColor:"gray",
+  },
+  taskTitle:{
+    fontSize:18
+  }
+});
